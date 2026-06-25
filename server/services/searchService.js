e06da5930d58c882a2f643e9e413e06bd3c65e85
brobrobrobrbr-png <brobrobrobrbr@gmail.com>
2026-06-25 23:15:08 +0300

    AI özelliği geri alındı, eski haline dönüldü

--- server/services/searchService.js
+++ server/services/searchService.js
@@ -1,28 +1,15 @@
 const { fetchYouTubeVideosByHashtag } = require("./youtubeService");
 const { generateMockVideos } = require("./mockDataService");
-const { generateInsights } = require("./insightsService");
 
 const USE_MOCK = process.env.USE_MOCK_DATA !== "false";
 const HAS_YOUTUBE_KEY = Boolean(process.env.YOUTUBE_API_KEY);
 
-// AI analizi opsiyoneldir: başarısız olursa arama sonucunu etkilememeli.
-async function safeGenerateInsights(hashtag, videos) {
-  try {
-    return await generateInsights(hashtag, videos);
-  } catch (err) {
-    console.error("[Insights Error]", err.message);
-    return null;
-  }
-}
-
 async function searchVideos(hashtag) {
   if (USE_MOCK || !HAS_YOUTUBE_KEY) {
-    const videos = generateMockVideos(hashtag);
     return {
       hashtag,
       isMock: true,
-      videos,
-      insights: await safeGenerateInsights(hashtag, videos),
+      videos: generateMockVideos(hashtag),
     };
   }
 
@@ -32,7 +19,6 @@ async function searchVideos(hashtag) {
     hashtag,
     isMock: false,
     videos,
-    insights: await safeGenerateInsights(hashtag, videos),
   };
 }
 
